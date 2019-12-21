3f7cb73aaaaab83acefbe3401db18ed2592b53ed
JK <kotharijk@gmail.com>
2019-12-21 16:40:13 -0500

    Added support for decimal

--- Power_x_n/Program.cs
+++ Power_x_n/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Power_x_n
 {
     /// <summary>
-    /// This class demonstrates how to calculate x raised to the power n using Divide & Conquer algorithm
+    /// This class demonstrates how to calculate x raised to the power n using an algorithm optimized for optimized for O(log n)
     /// </summary>
     class Program
     {
@@ -17,25 +17,32 @@ namespace Power_x_n
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            double result;
-            int num, exponent;
+            decimal result, num;
+            int exponent;
 
             // Test 1
             num = 2;
-            exponent = 3;
-            result = calcPower(num, exponent); // result should be 8
+            exponent = 10;
+            result = calcPower(num, exponent); // result should be 1024
             PrintResult(num, exponent, result);
 
             // Test 2
+            num = 3.5M;
+            exponent = 15;
+            result = calcPower(num, exponent); // result should be 144,884,079.282928466796875
+            PrintResult(num, exponent, result);
+
+
+            // Test 3
             num = 2;
-            exponent = 25;
-            result = calcPower(num, exponent); // result should be 33,554,432
+            exponent = 50;
+            result = calcPower(num, exponent); // result should be 1,125,899,906,842,624
             PrintResult(num, exponent, result);
 
             // Test 3
-            num = 5;
-            exponent = 7;
-            result = calcPower(num, exponent); // result should be 78125
+            num = 5.5M;
+            exponent = 15;
+            result = calcPower(num, exponent); // result should be 127,479,497,357.655364990234375
             PrintResult(num, exponent, result);
 
             // Test 4
@@ -49,17 +56,17 @@ namespace Power_x_n
 
         /// <summary>
         /// Function to calculate x raised to the power n using Divide & Conquer algorithm.
-        /// It is optimized to O(log n)
+        /// It is optimized for O(log n)
         /// </summary>
-        /// <param name="num">A number</param>
-        /// <param name="exponent">An Exponent</param>
-        /// <returns>Result of x raised to the power n</returns>
-        private static double calcPower(int num, int exponent)
+        /// <param name="num">A decimal value</param>
+        /// <param name="exponent">An integer value</param>
+        /// <returns>Result of num raised to the power exponent</returns>
+        private static decimal calcPower(decimal num, int exponent)
         {
             if (exponent == 0)
                 return 1;
 
-            double tempValue = calcPower(num, exponent / 2);
+            decimal tempValue = calcPower(num, exponent / 2);
 
             if (exponent % 2 == 0)
                 return tempValue * tempValue;
@@ -73,9 +80,9 @@ namespace Power_x_n
         /// <param name="num">A Number</param>
         /// <param name="exponent">An Exponent</param>
         /// <param name="result">Result of x raised to the power n</param>
-        private static void PrintResult(int num, int exponent, double result)
+        private static void PrintResult(decimal num, int exponent, decimal result)
         {
-            Console.WriteLine($"{num} raised to the power {exponent} = {result}.");
+            Console.WriteLine($"{num} raised to the power {exponent} = {result}");
         }
     }
 }
